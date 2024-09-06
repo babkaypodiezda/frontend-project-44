cdb15248cfc25cd7c465a348e4e0ff7603153b7c
babkaypodiezda <buninurik97@gmail.com>
2024-09-06 18:08:35 +0300

    switch path for games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 //import brainEven from '../games/brain-even.js';
-import brainCalc from '../games/brain-calc.js';
+import brainCalc from '../games/brain-calc.jsjs';
 
 
 //brainEven();

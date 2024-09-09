e867fcf480b5a0bf8c6efaea4116261aed7de679
babkaypodiezda <buninurik97@gmail.com>
2024-09-09 19:56:48 +0300

    add brain-progression

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 //import brainEven from '../games/brain-even.js';
 //import brainCalc from '../games/brain-calc.js';
-import brainGdc from '../games/brain-gdc.js';
-
-brainGdc();
+//import brainGdc from '../games/brain-gdc.js';
+import brainProgression from '../games/brain-progression.js';
+brainProgression();
---
+++ games/brain-progression.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import _ from 'lodash';
+
+export default () => {
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');  
+
+console.log('What number is missing in the progression?');
+
+
+for (let i = 0; i < 3; i += 1) {
+    let x = _.random(10); //start
+    let y = _.random(10); // increment
+    const arr = [];
+    arr.push(x);
+
+    for (let i = 0; i < 9; i += 1){
+        x += y;
+        arr.push(x);
+    }
+    let z = _.random(9);
+    let q = arr[z];
+    arr[z] = `..`;
+    console.log(`Questiom: ${arr}`);
+
+const answer = Number(readlineSync.question("Your answer: "));
+if (Number.isNaN(answer)){return console.log("Input only numbers")};
+
+if ( answer === q ) {
+    console.log("Correct!");
+    }
+else {
+    return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${q}'. Let's try again, ${userName}!`)
+    };
+}
+console.log(`Congratulations, ${userName}!`);
+}

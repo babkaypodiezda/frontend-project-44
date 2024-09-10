a16fd6afa607d9679001e71689489a9deac2931d
babkaypodiezda <buninurik97@gmail.com>
2024-09-10 17:47:31 +0300

    fix even

--- games/brain-even.js
+++ games/brain-even.js
@@ -4,9 +4,9 @@ import _ from 'lodash';
 
 export default () => {
 console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if number odd otherwise answer "no".\n');
 const userName = readlineSync.question('May I have your name?\n');
-console.log('Hello, ' + userName + '!');      
+console.log('Hello, ' + userName + '!');    
+console.log('Answer "yes" if the number is even, otherwise answer "no".');  
 
 for (let i = 0; i < 3; i += 1) {
     let x = _.random(100);

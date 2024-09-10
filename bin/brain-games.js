2bc6eae6f35ea3333d3823dc850897f9a40523bf
babkaypodiezda <buninurik97@gmail.com>
2024-09-10 17:20:16 +0300

    fix brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-//import brainEven from '../games/brain-even.js';
-//import brainCalc from '../games/brain-calc.js';
-//import brainGdc from '../games/brain-gdc.js';
-//import brainProgression from '../games/brain-progression.js';
-import brainPrime from '../games/brain-prime.js';
-brainPrime();
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');  
